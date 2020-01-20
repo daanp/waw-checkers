1cdbb0c4b3d63e9ef371c6987b212003c595cc83
Daan Paardekooper <daanp@Daans-MacBook-Pro.local>
2020-01-20 11:53:49 +0100

    find initial position of Jafar

--- solution.js
+++ solution.js
@@ -1,5 +1,13 @@
 function solution(board) {
-    return 1;
+
+    //Get initial position of Jafar's pawn
+    let position = {};
+    for (let i = 0; i < board.length; i++) {
+        if (board[i].indexOf('O') >= 0) {
+            position.x = board[i].indexOf('O');
+            position.y = i;
+        }
+    }
 }
 
 module.exports = solution;
